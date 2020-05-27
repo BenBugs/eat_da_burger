00667518eaec3d084c17e2668753449f5c0561ac
Ben Cole <benedictcole1@gmail.com>
2020-05-27 10:34:27 +0930

    added first sequelize model burger_type.js

---
+++ models/burger_type.js
@@ -0,0 +1,13 @@
+// 
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  const burger_type = sequelize.define('burger_type', {
+    burger_name: DataTypes.STRING,
+    price: DataTypes.STRING
+  }, {});
+  burger_type.associate = function(models) {
+    // associations can be defined here
+  };
+  return burger_type;
+};
+
